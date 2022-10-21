19576ad690230d36e457de6c7caefab558068216
Juan Mendes <mendejuan@gmail.com>
2022-10-21 15:08:41 -0400

    Lab 2 - Adding custom content to angular app
    Testing:
      * `nx serve store`
      * Ensure tha app loads and you see three cards for each of the games

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 import { appRoutes } from './app.routes';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    MatCardModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
