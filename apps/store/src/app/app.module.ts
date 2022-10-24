01fc80fa6bcd0201b5b9d53df8a294809974f88f
Juan Mendes <mendejuan@gmail.com>
2022-10-24 14:49:51 -0400

    Lab 4 - Make app.component use the new header componenbt
    Manual changes

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { appRoutes } from './app.routes';
+import {StoreUiSharedModule} from "@bg-hoard/store/ui-shared";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -13,6 +14,7 @@ import { appRoutes } from './app.routes';
     BrowserModule,
     MatCardModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    StoreUiSharedModule
   ],
   providers: [],
   bootstrap: [AppComponent],
