f9b51d1c61734574d9344ac60118fb28104de9e7
Juan Mendes <mendejuan@gmail.com>
2022-10-25 12:06:04 -0400

    Lab 6 - Generating routed library's component
    nx generate @nrwl/angular:component game-detail --project=store-feature-game-detail

---
+++ libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bg-hoard-game-detail',
+  templateUrl: './game-detail.component.html',
+  styleUrls: ['./game-detail.component.css'],
+})
+export class GameDetailComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- libs/store/feature-game-detail/src/lib/store-feature-game-detail.module.ts
+++ libs/store/feature-game-detail/src/lib/store-feature-game-detail.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { storeFeatureGameDetailRoutes } from './lib.routes';
-
+import { GameDetailComponent } from './game-detail/game-detail.component';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(storeFeatureGameDetailRoutes)],
+  imports: [
+    MatCardModule,
+    CommonModule,
+    RouterModule.forChild(storeFeatureGameDetailRoutes),
+  ],
+  declarations: [GameDetailComponent],
 })
 export class StoreFeatureGameDetailModule {}
