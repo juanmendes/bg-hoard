ddf84a47a85a8da815e6c40a694f395996117913
Juan Mendes <mendejuan@gmail.com>
2022-10-25 21:55:38 -0400

    Lab 8 - Manual changes to use /api/games
    Test by going to
      * http://localhost:3333/api/games
      * http://localhost:3333/api/games/settlers-in-the-can
    
    And seeing the mock JSON

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { appRoutes } from './app.routes';
-import {StoreUiSharedModule} from "@bg-hoard/store/ui-shared";
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -14,7 +14,8 @@ import {StoreUiSharedModule} from "@bg-hoard/store/ui-shared";
     BrowserModule,
     MatCardModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
-    StoreUiSharedModule
+    HttpClientModule,
+    StoreUiSharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
