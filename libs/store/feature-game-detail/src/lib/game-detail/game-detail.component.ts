b25bcf801a984403d02c9d1e8db3bc5fe3a5fc28
Juan Mendes <mendejuan@gmail.com>
2022-10-25 21:34:27 -0400

    Lab 6 - Manual changes to generated detail component to display details

--- libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.ts
+++ libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'bg-hoard-game-detail',
   templateUrl: './game-detail.component.html',
   styleUrls: ['./game-detail.component.css'],
 })
-export class GameDetailComponent implements OnInit {
-  constructor() {}
+export class GameDetailComponent {
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  gameId$ = this.route.paramMap.pipe(
+    map((params: ParamMap) => params.get('id'))
+  );
 }
